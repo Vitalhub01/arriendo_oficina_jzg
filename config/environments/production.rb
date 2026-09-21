69538baaa5ffa518530110ecd9951cd7ce4e73df
miguelo49 <miguel_z94@hotmail.com>
2026-09-20 23:11:30 -0300

    Update service host configuration for deployment
    - Changed the APP_HOST value in render.yaml to arriendo-oficina-jzg.onrender.com.
    - Added arriendo-oficina-jzg.onrender.com to the allowed hosts in production.rb for Rails application.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
 
   config.hosts << 'arriendodebox.cl'
   config.hosts << 'rentabox.onrender.com'
+  config.hosts << 'arriendo-oficina-jzg.onrender.com'
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
